ef2d47540039da3f7ae360626f9eb17a8452ac24
Kevin Broodkoorn <kb@taktik.com>
2016-03-22 11:37:59 +0100

    ADD tk_pos_order_reprint

---
+++ tk_pos_order_reprint/static/src/js/wk_order_repaint_main.js
@@ -0,0 +1,5 @@
+openerp.pos_order_repaint = function (instance) {
+    var module = instance.point_of_sale;
+    console.log("this is for test");
+    wk_openerp_pos_order_repaint(instance, module);    // import pos_devices.js
+};
