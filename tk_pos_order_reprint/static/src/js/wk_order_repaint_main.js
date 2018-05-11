7d54ceb82cf3652d2cb27a212c622052a4db2d59
Alexandre Pieroux <alex.pieroux@gmail.com>
2018-05-11 11:29:41 +0200

    [MIGRATION][INIT]: Init migration branche for module 8.0 to 10.0

--- tk_pos_order_reprint/static/src/js/wk_order_repaint_main.js
+++
@@ -1,5 +0,0 @@
-openerp.pos_order_repaint = function (instance) {
-    var module = instance.point_of_sale;
-    console.log("this is for test");
-    wk_openerp_pos_order_repaint(instance, module);    // import pos_devices.js
-};
